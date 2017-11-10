67db18bf003c2051d7c57eb513205a33d7a04e2f
Roland Askew <rolandjamesaskew37@gmail.com>
2017-11-11 12:14:49 +1300

    inc

--- static/local.js
+++ static/local.js
@@ -8,12 +8,11 @@ var THEME = {};
 
 function window_resize() = {};
 
-THEME.window_resize = $(function() {
-    $('.content-main').removeClass('content-bottom-normal').removeClass('content-bottom-narrow').addClass('content-bottom-normal');
+THEME.window_resize = function() {
     if(window.innerWidth < 768) {
         //$('.content-main').removeClass('content-bottom-normal').addClass('content-bottom-narrow');
     } else {
         //$('.content-main').removeClass('content-bottom-narrow').addClass('content-bottom-normal');
     };
     window_resize();
-});
+};
